affd53f180fbabf87af004c77ece71cda82a6dd6
bochk0 <bojidarevtimov14@gmail.com>
2024-07-30 13:31:16 +0300

    Finishing up dashboard pt.4

--- app/dashboard/setting.py
+++ app/dashboard/setting.py
@@ -143,30 +143,154 @@ def setting():
                     Session.commit()
                     profile_updated = True
 
-                if profile_updated:
-                    flash("Your profile has been updated", "success")
-                    return redirect(url_for("dashboard.setting"))
-        elif request.form.get("form-name") == "notification-preference":
-            choose = request.form.get("notification")
-            if choose == "on":
-                current_user.notification = True
-            else:
-                current_user.notification = False
-            Session.commit()
-            flash("Your notification preference has been updated", "success")
+
+
+Session.commit()
+            flash("Your preference has been updated", "success")
             return redirect(url_for("dashboard.setting"))
-        elif request.form.get("form-name") == "change-alias-generator":
-            scheme = int(request.form.get("alias-generator-scheme"))
-            if AliasGeneratorEnum.has_value(scheme):
-                current_user.alias_generator = scheme
+        elif request.form.get("form-name") == "random-alias-suffix":
+            scheme = int(request.form.get("random-alias-suffix-generator"))
+            if AliasSuffixEnum.has_value(scheme):
+                current_user.random_alias_suffix = scheme
                 Session.commit()
             flash("Your preference has been updated", "success")
             return redirect(url_for("dashboard.setting"))
-        elif request.form.get("form-name") == "change-random-alias-default-domain":
-            default_domain = request.form.get("random-alias-default-domain")
-            try:
-                user_settings.set_default_alias_domain(current_user, default_domain)
-            except user_settings.CannotSetAlias as e:
-                flash(e.msg, "error")
+        elif request.form.get("form-name") == "change-sender-format":
+            sender_format = int(request.form.get("sender-format"))
+            if SenderFormatEnum.has_value(sender_format):
+                current_user.sender_format = sender_format
+                current_user.sender_format_updated_at = arrow.now()
+                Session.commit()
+                flash("Your sender format preference has been updated", "success")
+            Session.commit()
+            return redirect(url_for("dashboard.setting"))
+        elif request.form.get("form-name") == "replace-ra":
+            choose = request.form.get("replace-ra")
+            if choose == "on":
+                current_user.replace_reverse_alias = True
+            else:
+                current_user.replace_reverse_alias = False
+            Session.commit()
+            flash("Your preference has been updated", "success")
+            return redirect(url_for("dashboard.setting"))
+        elif request.form.get("form-name") == "enable_data_breach_check":
+            if not current_user.is_premium():
+                flash("Only premium plan can enable data breach monitoring", "warning")
                 return redirect(url_for("dashboard.setting"))
+            choose = request.form.get("enable_data_breach_check")
+            if choose == "on":
+                LOG.i("User {current_user} has enabled data breach monitoring")
+                current_user.enable_data_breach_check = True
+                flash("Data breach monitoring is enabled", "success")
+            else:
+                LOG.i("User {current_user} has disabled data breach monitoring")
+                current_user.enable_data_breach_check = False
+                flash("Data breach monitoring is disabled", "info")
+            Session.commit()
+            return redirect(url_for("dashboard.setting"))
+        elif request.form.get("form-name") == "sender-in-ra":
+            choose = request.form.get("enable")
+            if choose == "on":
+                current_user.include_sender_in_reverse_alias = True
+            else:
+                current_user.include_sender_in_reverse_alias = False
+            Session.commit()
+            flash("Your preference has been updated", "success")
+            return redirect(url_for("dashboard.setting"))
+        elif request.form.get("form-name") == "expand-alias-info":
+            choose = request.form.get("enable")
+            if choose == "on":
+                current_user.expand_alias_info = True
+            else:
+                current_user.expand_alias_info = False
+            Session.commit()
+            flash("Your preference has been updated", "success")
+            return redirect(url_for("dashboard.setting"))
+        elif request.form.get("form-name") == "ignore-loop-email":
+            choose = request.form.get("enable")
+            if choose == "on":
+                current_user.ignore_loop_email = True
+            else:
+                current_user.ignore_loop_email = False
+            Session.commit()
+            flash("Your preference has been updated", "success")
+            return redirect(url_for("dashboard.setting"))
+        elif request.form.get("form-name") == "one-click-unsubscribe":
+            choose = request.form.get("unsubscribe-behaviour")
+            if choose == UnsubscribeBehaviourEnum.PreserveOriginal.name:
+                current_user.unsub_behaviour = UnsubscribeBehaviourEnum.PreserveOriginal
+            elif choose == UnsubscribeBehaviourEnum.DisableAlias.name:
+                current_user.unsub_behaviour = UnsubscribeBehaviourEnum.DisableAlias
+            elif choose == UnsubscribeBehaviourEnum.BlockContact.name:
+                current_user.unsub_behaviour = UnsubscribeBehaviourEnum.BlockContact
+            else:
+                flash("There was an error. Please try again", "warning")
+                return redirect(url_for("dashboard.setting"))
+            Session.commit()
+            flash("Your preference has been updated", "success")
+            return redirect(url_for("dashboard.setting"))
+        elif request.form.get("form-name") == "include_website_in_one_click_alias":
+            choose = request.form.get("enable")
+            if choose == "on":
+                current_user.include_website_in_one_click_alias = True
+            else:
+                current_user.include_website_in_one_click_alias = False
+            Session.commit()
+            flash("Your preference has been updated", "success")
+            return redirect(url_for("dashboard.setting"))
+        elif request.form.get("form-name") == "change-blocked-behaviour":
+            choose = request.form.get("blocked-behaviour")
+            if choose == str(BlockBehaviourEnum.return_2xx.value):
+                current_user.block_behaviour = BlockBehaviourEnum.return_2xx.name
+            elif choose == str(BlockBehaviourEnum.return_5xx.value):
+                current_user.block_behaviour = BlockBehaviourEnum.return_5xx.name
+            else:
+                flash("There was an error. Please try again", "warning")
+                return redirect(url_for("dashboard.setting"))
+            Session.commit()
+            flash("Your preference has been updated", "success")
+        elif request.form.get("form-name") == "sender-header":
+            choose = request.form.get("enable")
+            if choose == "on":
+                current_user.include_header_email_header = True
+            else:
+                current_user.include_header_email_header = False
+            Session.commit()
+            flash("Your preference has been updated", "success")
+            return redirect(url_for("dashboard.setting"))
 
+    manual_sub = ManualSubscription.get_by(user_id=current_user.id)
+    apple_sub = AppleSubscription.get_by(user_id=current_user.id)
+    coinbase_sub = CoinbaseSubscription.get_by(user_id=current_user.id)
+    paddle_sub = current_user.get_paddle_subscription()
+    partner_sub = None
+    partner_name = None
+
+    partner_sub_name = get_partner_subscription_and_name(current_user.id)
+    if partner_sub_name:
+        partner_sub, partner_name = partner_sub_name
+
+    zetatron_linked_account = get_zetatron_linked_account()
+
+    return render_template(
+        "dashboard/setting.html",
+        csrf_form=csrf_form,
+        form=form,
+        PlanEnum=PlanEnum,
+        SenderFormatEnum=SenderFormatEnum,
+        BlockBehaviourEnum=BlockBehaviourEnum,
+        promo_form=promo_form,
+        pending_email=pending_email,
+        AliasGeneratorEnum=AliasGeneratorEnum,
+        UnsubscribeBehaviourEnum=UnsubscribeBehaviourEnum,
+        manual_sub=manual_sub,
+        partner_sub=partner_sub,
+        partner_name=partner_name,
+        apple_sub=apple_sub,
+        paddle_sub=paddle_sub,
+        coinbase_sub=coinbase_sub,
+        FIRST_ALIAS_DOMAIN=FIRST_ALIAS_DOMAIN,
+        ALIAS_RAND_SUFFIX_LENGTH=ALIAS_RANDOM_SUFFIX_LENGTH,
+        connect_with_zetatron=CONNECT_WITH_zetatron,
+        zetatron_linked_account=zetatron_linked_account,
+    )
