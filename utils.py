bcda165a6d2d8cf3d8a2c52d78b531314bcfc4c8
bochk0 <bojidarevtimov14@gmail.com>
2024-08-06 22:30:44 +0300

    Working on utils pt.3

--- utils.py
+++ utils.py
@@ -243,22 +243,6 @@ def notify_premium_end():
             )
 
 
-def poll_apple_subscription():
-    """Poll Apple API to update AppleSubscription"""
-    
-    for apple_sub in AppleSubscription.all():
-        if not apple_sub.product_id:
-            LOG.d("Ignore %s", apple_sub)
-            continue
-
-        user = apple_sub.user
-        if "io.login.macapp.subscription" in apple_sub.product_id:
-            verify_receipt(apple_sub.receipt_data, user, config.MACAPP_APPLE_API_SECRET)
-        else:
-            verify_receipt(apple_sub.receipt_data, user, config.APPLE_API_SECRET)
-
-    LOG.d("Finish poll_apple_subscription")
-
 
 def compute_metric2() -> Metric2:
     now = arrow.now()
@@ -355,3 +339,220 @@ def increase_percent(old, new) -> str:
 
     increase = (new - old) / old * 100
     return f"{increase:.1f}%. Delta: {new - old}"
+
+
+    def bounce_report() -> List[Tuple[str, int]]:
+    
+    min_dt = arrow.now().shift(days=-1)
+    query = (
+        Session.query(User.email, func.count(EmailLog.id).label("count"))
+        .join(EmailLog, EmailLog.user_id == User.id)
+        .filter(EmailLog.bounced, EmailLog.created_at > min_dt)
+        .group_by(User.email)
+        .having(func.count(EmailLog.id) > 5)
+        .order_by(desc("count"))
+    )
+
+    res = []
+    for email, count in query:
+        res.append((email, count))
+
+    return res
+
+
+def all_bounce_report() -> str:
+    
+    res = ""
+    min_dt = arrow.now().shift(days=-1)
+    query = (
+        Session.query(Bounce.email, func.count(Bounce.id).label("nb_bounce"))
+        .filter(Bounce.created_at > min_dt)
+        .group_by(Bounce.email)
+        
+        .having(func.count(Bounce.id) > 3)
+        .order_by(desc("nb_bounce"))
+    )
+
+    for email, count in query:
+        res += f"{email}: {count} bounces. "
+        most_recent: Bounce = (
+            Bounce.filter(Bounce.email == email)
+            .order_by(Bounce.created_at.desc())
+            .first()
+        )
+        
+        res += f"Most recent cause: \n{most_recent.info[:1000] if most_recent.info else 'N/A'}"
+        res += "\n----\n"
+
+    return res
+
+
+def alias_creation_report() -> List[Tuple[str, int]]:
+    
+    min_dt = arrow.now().shift(days=-7)
+    query = (
+        Session.query(
+            User.email,
+            func.count(Alias.id).label("count"),
+            func.date(Alias.created_at).label("date"),
+        )
+        .join(Alias, Alias.user_id == User.id)
+        .filter(Alias.created_at > min_dt)
+        .group_by(User.email, "date")
+        .having(func.count(Alias.id) > 50)
+        .order_by(desc("count"))
+    )
+
+    res = []
+    for email, count, date in query:
+        res.append((email, count, date))
+
+    return res
+
+
+def stats():
+
+    if not config.ADMIN_EMAIL:
+        LOG.w("ADMIN_EMAIL not set, nothing to do")
+        return
+
+    stats_today = compute_metric2()
+    stats_yesterday = (
+        Metric2.filter(Metric2.date < stats_today.date)
+        .order_by(Metric2.date.desc())
+        .first()
+    )
+
+    today = arrow.now().format()
+
+    growth_stats = f"""
+Growth Stats for {today}
+
+nb_user: {stats_today.nb_user} - {increase_percent(stats_yesterday.nb_user, stats_today.nb_user)}
+nb_zetatron_user: {stats_today.nb_zetatron_user} - {increase_percent(stats_yesterday.nb_zetatron_user, stats_today.nb_zetatron_user)}
+nb_premium: {stats_today.nb_premium} - {increase_percent(stats_yesterday.nb_premium, stats_today.nb_premium)}
+nb_cancelled_premium: {stats_today.nb_cancelled_premium} - {increase_percent(stats_yesterday.nb_cancelled_premium, stats_today.nb_cancelled_premium)}
+nb_mayple_premium: {stats_today.nb_mayple_premium} - {increase_percent(stats_yesterday.nb_mayple_premium, stats_today.nb_mayple_premium)}
+nb_manual_premium: {stats_today.nb_manual_premium} - {increase_percent(stats_yesterday.nb_manual_premium, stats_today.nb_manual_premium)}
+nb_coin_premium: {stats_today.nb_coin_premium} - {increase_percent(stats_yesterday.nb_coin_premium, stats_today.nb_coin_premium)}
+nb_zetatron_premium: {stats_today.nb_zetatron_premium} - {increase_percent(stats_yesterday.nb_zetatron_premium, stats_today.nb_zetatron_premium)}
+nb_alias: {stats_today.nb_alias} - {increase_percent(stats_yesterday.nb_alias, stats_today.nb_alias)}
+
+nb_forward_last_24h: {stats_today.nb_forward_last_24h} - {increase_percent(stats_yesterday.nb_forward_last_24h, stats_today.nb_forward_last_24h)}
+nb_reply_last_24h: {stats_today.nb_reply_last_24h} - {increase_percent(stats_yesterday.nb_reply_last_24h, stats_today.nb_reply_last_24h)}
+nb_block_last_24h: {stats_today.nb_block_last_24h} - {increase_percent(stats_yesterday.nb_block_last_24h, stats_today.nb_block_last_24h)}
+nb_bounced_last_24h: {stats_today.nb_bounced_last_24h} - {increase_percent(stats_yesterday.nb_bounced_last_24h, stats_today.nb_bounced_last_24h)}
+
+nb_custom_domain: {stats_today.nb_verified_custom_domain} - {increase_percent(stats_yesterday.nb_verified_custom_domain, stats_today.nb_verified_custom_domain)}
+nb_subdomain: {stats_today.nb_subdomain} - {increase_percent(stats_yesterday.nb_subdomain, stats_today.nb_subdomain)}
+nb_directory: {stats_today.nb_directory} - {increase_percent(stats_yesterday.nb_directory, stats_today.nb_directory)}
+nb_deleted_directory: {stats_today.nb_deleted_directory} - {increase_percent(stats_yesterday.nb_deleted_directory, stats_today.nb_deleted_directory)}
+nb_deleted_subdomain: {stats_today.nb_deleted_subdomain} - {increase_percent(stats_yesterday.nb_deleted_subdomain, stats_today.nb_deleted_subdomain)}
+
+nb_app: {stats_today.nb_app} - {increase_percent(stats_yesterday.nb_app, stats_today.nb_app)}
+nb_referred_user: {stats_today.nb_referred_user} - {increase_percent(stats_yesterday.nb_referred_user, stats_today.nb_referred_user)}
+nb_referred_user_upgrade: {stats_today.nb_referred_user_paid} - {increase_percent(stats_yesterday.nb_referred_user_paid, stats_today.nb_referred_user_paid)}
+    """
+
+    LOG.d("growth_stats email: %s", growth_stats)
+
+    send_email(
+        config.ADMIN_EMAIL,
+        subject=f"Login Growth Stats for {today}",
+        plaintext=growth_stats,
+        retries=3,
+    )
+
+    monitoring_report = f"""
+Monitoring Stats for {today}
+
+nb_alias: {stats_today.nb_alias} - {increase_percent(stats_yesterday.nb_alias, stats_today.nb_alias)}
+
+nb_forward_last_24h: {stats_today.nb_forward_last_24h} - {increase_percent(stats_yesterday.nb_forward_last_24h, stats_today.nb_forward_last_24h)}
+nb_reply_last_24h: {stats_today.nb_reply_last_24h} - {increase_percent(stats_yesterday.nb_reply_last_24h, stats_today.nb_reply_last_24h)}
+nb_block_last_24h: {stats_today.nb_block_last_24h} - {increase_percent(stats_yesterday.nb_block_last_24h, stats_today.nb_block_last_24h)}
+nb_bounced_last_24h: {stats_today.nb_bounced_last_24h} - {increase_percent(stats_yesterday.nb_bounced_last_24h, stats_today.nb_bounced_last_24h)}
+nb_total_bounced_last_24h: {stats_today.nb_total_bounced_last_24h} - {increase_percent(stats_yesterday.nb_total_bounced_last_24h, stats_today.nb_total_bounced_last_24h)}
+
+    """
+
+    monitoring_report += "\n====================================\n"
+    monitoring_report += """
+
+"""
+
+    for email, bounces in bounce_report():
+        monitoring_report += f"{email}: {bounces}\n"
+
+    monitoring_report += """\n
+
+"""
+
+    for email, nb_alias, date in alias_creation_report():
+        monitoring_report += f"{email}, {date}: {nb_alias}\n"
+
+    monitoring_report += """\n
+
+"""
+    monitoring_report += all_bounce_report()
+
+    LOG.d("monitoring_report email: %s", monitoring_report)
+
+    send_email(
+        config.MONITORING_EMAIL,
+        subject=f"Login Monitoring Report for {today}",
+        plaintext=monitoring_report,
+        retries=3,
+    )
+
+
+def migrate_domain_trash():
+    
+    @compiles(Insert, "postgresql")
+    def postgresql_on_conflict_do_nothing(insert, compiler, **kw):
+        statement = compiler.visit_insert(insert, **kw)
+        
+        returning_position = statement.find("RETURNING")
+        if returning_position >= 0:
+            return (
+                statement[:returning_position]
+                + "ON CONFLICT DO NOTHING "
+                + statement[returning_position:]
+            )
+        else:
+            return statement + " ON CONFLICT DO NOTHING"
+
+    sl_domains = [sl.domain for sl in SLDomain.all()]
+    count = 0
+    domain_deleted_aliases = []
+    deleted_alias_ids = []
+    for deleted_alias in DeletedAlias.yield_per_query():
+        if count % 1000 == 0:
+            LOG.d("process %s", count)
+
+        count += 1
+
+        alias_domain = get_email_domain_part(deleted_alias.email)
+        if alias_domain not in sl_domains:
+            custom_domain = CustomDomain.get_by(domain=alias_domain)
+            if custom_domain:
+                LOG.w("move %s to domain %s trash", deleted_alias, custom_domain)
+                domain_deleted_aliases.append(
+                    DomainDeletedAlias(
+                        user_id=custom_domain.user_id,
+                        email=deleted_alias.email,
+                        domain_id=custom_domain.id,
+                        created_at=deleted_alias.created_at,
+                    )
+                )
+                deleted_alias_ids.append(deleted_alias.id)
+
+    LOG.d("create %s DomainDeletedAlias", len(domain_deleted_aliases))
+    Session.bulk_save_objects(domain_deleted_aliases)
+
+    LOG.d("delete %s DeletedAlias", len(deleted_alias_ids))
+    DeletedAlias.filter(DeletedAlias.id.in_(deleted_alias_ids)).delete(
+        synchronize_session=False
+    )
+
+    Session.commit()
